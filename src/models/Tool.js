4d80edc6d1451cb33d67848029dc5b6ef23cd7ec
Thiago Lira <lira_thiago@msn.com>
2021-06-26 17:48:54 -0300

    validate Tool instantiation

--- src/models/Tool.js
+++ src/models/Tool.js
@@ -1,7 +1,15 @@
+import Tag from '@/models/Tag';
+
 class Tool {
   constructor({
     name, link, description, tags,
   }) {
+    const hasInvalidTag = tags.some((tag) => !(tag instanceof Tag));
+
+    if (hasInvalidTag) {
+      throw new Error('All tags must to be a Tag object');
+    }
+
     Object.assign(this, {
       name, link, description, tags,
     });
