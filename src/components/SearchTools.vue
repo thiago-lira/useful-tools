71ece33e140e122b47adb55b84815602d4ef5eb0
Thiago Lira <lira_thiago@msn.com>
2021-06-28 22:24:57 -0300

    create SearchTools component

---
+++ src/components/SearchTools.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="task-bar__search">
+    <input
+      type="text"
+      placeholder="Buscar ferramenta"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+
+    <label>
+      <input type="checkbox" />
+      apenas tags
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchTools',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
--- src/components/TaskBar.vue
+++ src/components/TaskBar.vue
@@ -1,18 +1,9 @@
 <template>
   <section class="task-bar">
-    <div class="task-bar__search">
-      <input
-        type="text"
-        placeholder="Buscar ferramenta"
-        :value="value"
-        @input="$emit('input', $event.target.value)"
-      >
-
-      <label>
-        <input type="checkbox">
-        apenas tags
-      </label>
-    </div>
+    <SearchTools
+      v-model="searchValue"
+      @input="$emit('input', searchValue)"
+    />
 
     <div class="task-bar__add-tool">
       <button @click="handleAddClick" data-cy="btn-add-tool">
@@ -29,13 +20,23 @@
 </template>
 
 <script>
+import SearchTools from '@/components/SearchTools.vue';
+
 export default {
   name: 'TaskBar',
+  components: {
+    SearchTools,
+  },
   props: {
     value: {
       type: String,
       default: '',
     },
+  },
+  data() {
+    return {
+      searchValue: this.value,
+    };
   },
   methods: {
     handleAddClick() {
